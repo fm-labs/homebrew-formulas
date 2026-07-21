d38dc8fbcee7d257e10dfafedb6ba7df92bf285c
fm-labs <flowmotionlabs@gmail.com>
2026-07-21 10:35:01 +0200

    geenii v0.3.2-rc.3

--- geenii.rb
+++ geenii.rb
@@ -1,12 +1,12 @@
 class Geenii < Formula
   desc "geenii"
   homepage "https://github.com/fm-labs/geenii"
-  version "0.3.2-rc.2"
+  version "0.3.2-rc.3"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/fm-labs/geenii/releases/download/v0.3.2-rc.2/geenii-darwin-amd64.tar.gz"
-      sha256 "c86ae58d601a73303b3633f6c925e456ea8d433d234e8e56ca70ffef98fd202c"
+      url "https://github.com/fm-labs/geenii/releases/download/v0.3.2-rc.3/geenii-darwin-amd64.tar.gz"
+      sha256 "e9744f3e7c4e2854b5d3f8f2e88abe6e743e4cae912643d374b0a85555f53b49"
 
       def install
         libexec.install Dir["*"]
@@ -14,8 +14,8 @@ class Geenii < Formula
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/fm-labs/geenii/releases/download/v0.3.2-rc.2/geenii-darwin-arm64.tar.gz"
-      sha256 "e42c6fc4e54517064c08db329f369c593bc2e921a7fd1796b789b495881f4dac"
+      url "https://github.com/fm-labs/geenii/releases/download/v0.3.2-rc.3/geenii-darwin-arm64.tar.gz"
+      sha256 "6130aadfc50125fb96f2c6e6e8a2a397eb5cfda2e04895265b76c58ec02409cd"
 
       def install
         libexec.install Dir["*"]
@@ -27,8 +27,8 @@ class Geenii < Formula
   on_linux do
     if Hardware::CPU.intel?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/fm-labs/geenii/releases/download/v0.3.2-rc.2/geenii-linux-amd64.tar.gz"
-        sha256 "7121eedff7a730198ee4317e5f8291822ddbdf10ea3f130d36f3215f47b88aaf"
+        url "https://github.com/fm-labs/geenii/releases/download/v0.3.2-rc.3/geenii-linux-amd64.tar.gz"
+        sha256 "9eb1aa2ede5be65c490d420e97d545f1150e6de24da9ccf6bba815d72103104a"
 
         def install
           libexec.install Dir["*"]
@@ -38,8 +38,8 @@ class Geenii < Formula
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/fm-labs/geenii/releases/download/v0.3.2-rc.2/geenii-linux-arm64.tar.gz"
-        sha256 "3f3e0d11319f61978a86ffc08d4674289143f5da23a51cc09cee2db87b4b98dd"
+        url "https://github.com/fm-labs/geenii/releases/download/v0.3.2-rc.3/geenii-linux-arm64.tar.gz"
+        sha256 "ddf2067d15e2fe7dc35f1bced3c14f1a2971932425a29c1bdbb6a23df157909d"
 
         def install
           libexec.install Dir["*"]
